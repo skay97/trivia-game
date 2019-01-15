552905b65505a72d39120ce6b37fd9ab7a6de203
Salman Karim <Lego@MacBook-Pro-2.local>
2019-01-15 16:33:00 -0600

    update

---
+++ assets/javascript/questions.js
@@ -0,0 +1,36 @@
+var questions = [{
+    "question": "What is Captain America's shield made out off?",
+    "option1": "Titanium",
+    "option2": "Vibranium",
+    "option3": "Wakandan Micro Ally",
+    "option4": "Steel",
+    "answer": "2"
+}, {
+    "question": "What is the name of the Technology that keeps Tony Stark alive?",
+    "option1": "Chronometer",
+    "option2": "Tachymetere Chrome",
+    "option3": "Arc Reactor",
+    "option4": "Tourbillion",
+    "answer": "3"
+}, {
+    "question": "What is Star Lord's Real name?",
+    "option1": "Peter Quill",
+    "option2": "Nick Fury",
+    "option3": "Wade Wilson",
+    "option4": "Peter Parker",
+    "answer": "1"
+}, {
+    "question": "How many infinity stone exist in the MCU",
+    "option1": "6",
+    "option2": "5",
+    "option3": "4",
+    "option4": "3",
+    "answer": "1"
+}, {
+    "question": "What is the mystic cube that Red Skull trie to use to conquer the World?",
+    "option1": "The Orb",
+    "option2": "The Aether",
+    "option3": "The Tesseract",
+    "option4": "The Time Stone",
+    "answer": "3"
+}]
